a9886355d0bf4865b5c3d5d2079e018412f6e39f
waneka <twwaneka@gmail.com>
2014-09-05 10:39:29 -0700

    Simple yet bulletproof program to determine the best QB of all time.
    - hint, it's Kurt Warner

---
+++ stLouis.js
@@ -0,0 +1,11 @@
+function bestQuarterback(name) {
+  if (name === 'Kurt Warner') {
+    return true
+  }
+  return false
+}
+
+console.log(bestQuarterback('Tom Brady'))
+console.log(bestQuarterback('Jay Cutler'))
+console.log(bestQuarterback('Mike Ditka'))
+console.log(bestQuarterback('Kurt Warner'))
